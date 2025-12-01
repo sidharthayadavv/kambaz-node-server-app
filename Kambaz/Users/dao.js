e1fc0b0a7e99226f45f66c4788d4b94e1c2c29ba
Sidhartha Yadav Vadlakonda <sidharthayadav@Sidharthas-MacBook-Pro.local>
2025-12-01 11:14:57 -0500

    a6 first commit

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -1,22 +1,35 @@
 import { v4 as uuidv4 } from "uuid";
-export default function UsersDao(db) {
-    let { users } = db;
-    const createUser = (user) => {
-        const newUser = { ...user, _id: uuidv4(), role: user.role || "STUDENT" };
-        users = [...users, newUser];
-        return newUser;
-    };
-
-    const findAllUsers = () => users;
-    const findUserById = (userId) => users.find((user) => user._id === userId);
-    const findUserByUsername = (username) => users.find((user) => user.username === username);
-    const findUserByCredentials = (username, password) =>
-        users.find((user) => user.username === username && user.password === password);
-    const updateUser = (userId, user) => (users = users.map((u) => (u._id === userId ? user : u)));
-
-
-    const deleteUser = (userId) => (users = users.filter((u) => u._id !== userId));
-    return {
-        createUser, findAllUsers, findUserById, findUserByUsername, findUserByCredentials, updateUser, deleteUser
-    };
+import model from "./model.js";
+export default function UsersDao() {
+  const createUser = (user) => {
+    const newUser = { ...user, _id: uuidv4(), role: user.role || "STUDENT" };
+    return model.create(newUser);
+  };
+  const findUsersByRole = (role) => model.find({ role: role });
+  const findAllUsers = () => model.find();
+  const findUserById = (userId) => model.findById(userId);
+  const findUserByUsername = (username) =>
+    model.findOne({ username: username });
+  const findUserByCredentials = (username, password) =>
+    model.findOne({ username, password });
+  const updateUser = (userId, user) =>
+    model.updateOne({ _id: userId }, { $set: user });
+  const deleteUser = (userId) => model.findByIdAndDelete( userId );
+  const findUsersByPartialName = (partialName) => {
+    const regex = new RegExp(partialName, "i");
+    return model.find({
+      $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
+    });
+  };
+  return {
+    createUser,
+    findAllUsers,
+    findUserById,
+    findUserByUsername,
+    findUserByCredentials,
+    updateUser,
+    deleteUser,
+    findUsersByRole,
+    findUsersByPartialName,
+  };
 }
--- index.js
+++ index.js
@@ -10,7 +10,10 @@ import CourseRoutes from './Kambaz/Courses/routes.js';
 import ModuleRoutes from './Kambaz/Modules/routes.js';
 import AssignmentRoutes from './Kambaz/Assignments/routes.js';
 import EnrollmentRoutes from "./Kambaz/Enrollments/routes.js";
+import mongoose from "mongoose";
 
+const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 
 const isProduction = process.env.NODE_ENV === 'production';
